1d5f2a86fe5d0a8a758f3357370d08ffffd60778
ikhuram <ikhuram@yahoo.com>
2019-08-25 11:32:34 +0300

    Project Initialized

---
+++ Ecart.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ecart.Web.MvcApplication" Language="C#" %>
---
+++ Ecart.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ecart.Web.Startup))]
+namespace Ecart.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
